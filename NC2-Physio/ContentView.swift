2092672b63fafdf5a5d6cf4972b414de2343c18b
wgaliano <wgaliano22@fed.idserve.net>
2022-12-07 11:04:49 +0100

    first commit

--- NC2-Physio/ContentView.swift
+++ NC2-Physio/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
+        Text("Walter")
         NavigationView {
             List {
                 ForEach(items) { item in
