9c6da821b6c2878c370af2fa8182548ff5cb38f6
wgaliano <wgaliano22@fed.idserve.net>
2022-12-07 10:59:10 +0100

    Initial Commit

---
+++ NC2-Physio/NC2_PhysioApp.swift
@@ -0,0 +1,20 @@
+//
+//  NC2_PhysioApp.swift
+//  NC2-Physio
+//
+//  Created by Walter Galiano on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NC2_PhysioApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
